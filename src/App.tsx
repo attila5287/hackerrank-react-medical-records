d5a55842ba4b7fd94262001bf70443281880e0f6
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-20 06:44:03 -0600

    commit before charts.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,63 @@ import data from "./data"; // Make sure data.tsx exports `default data`
 import Header from "./components/Header";
 import "./App.css";
 import { useState } from "react";
+import "bootswatch/dist/slate/bootstrap.min.css";
 
+function UserGeneralInfo({ record }: { record: any }) {
+  return (
+    <ul className="list-group text-sm list-sm" style={{ textAlign: "left" }}>
+      <li className="list-group-item">Name: {record.userName}</li>
+      <li className="list-group-item">DOB: {record.userDob}</li>
+      <li className="list-group-item">Height: {record.meta.height}</li>
+    </ul>
+  );
+}
+function RecordDiagnosis({ record }: { record: any }) {
+  return (
+    <ul className="list-group text-sm list-sm" style={{ textAlign: "left" }}>
+      <li className="list-group-item">ID: {record.diagnosis.id}</li>
+      <li className="list-group-item">Name: {record.diagnosis.name}</li>
+      <li className="list-group-item">Severity: {record.diagnosis.severity}</li>
+    </ul>
+  );
+}
+function UserVitals({ record }: { record: any }) {
+  return (
+    <ul className="list-group" style={{ textAlign: "left" }} >
+      {Object.keys(record.vitals).map((key: string) => (
+        <li className="list-group-item">{key}: {record.vitals[key]}</li>
+      ))}
+    </ul>
+  )
+}
+function RecordInfo({ record }: { record: any }) {
+  return (
+    <ul className="list-group" style={{ textAlign: "left" }} >
+      <li className="list-group-item">
+        <i className="fa-solid fa-calendar-days me-2"></i>
+        Record ID: {record.id}
+      </li>
+      <li className="list-group-item">
+        <i className="fa-solid fa-calendar-days me-2"></i>
+        Record Date: {new Date(record.timestamp).toLocaleDateString()}
+      </li>
+      <li className="list-group-item">
+        <i className="fa-solid fa-user-doctor me-2"></i>
+        Doctor: {record.doctor.name}
+      </li>
+      <li className="list-group-item">
+        <i className="fa-solid fa-weight-scale me-2"></i>
+        Weight: {record.meta.weight}
+      </li>
+    </ul>
+  )
+}
 function App() {
   const [selectedUserIndex, setSelectedUserIndex] = useState<number>(0);
   const [ selectedRecordIndex, setSelectedRecordIndex] = useState<number>(0);
   const userIds = data.map((d: any) => d.id)
   const userData = data.filter((d: any) => d.id === userIds[selectedUserIndex])[0]
-  console.log(...userData.data)
+  // console.log(...userData.data)
 
   const handleUserClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const increment = parseInt(e.currentTarget.dataset.increment || "0");
@@ -24,40 +74,95 @@ function App() {
       setSelectedRecordIndex(newRecordIndex)
     }
   };
+  const selectedRecord = data.filter((d: any) => d.id === userIds[selectedUserIndex])[0].data[
+    selectedRecordIndex
+  ];
+  console.log(selectedRecord)
   return (
     <>
       <Header />
-      <button
-        key="prevUser"
-        data-increment={-1}
-        onClick={handleUserClick}
-      >
-        Prev Patient
-      </button>
-      <button key="nextUser"
-        data-increment={1}
-        onClick={handleUserClick}
-      >
-          Next Patient
-      </button>
-      <br />
-      <img style={{width: "100px", height: "100px"}} src={"./images/" + userIds[selectedUserIndex] + ".jpg"} alt="User" />
-      <h4 className="no-margin">User Index: {selectedUserIndex}</h4>
-      <h4 className="no-margin">User ID: {userIds[selectedUserIndex]}</h4>
-      <button key="prevRecord"
-        data-increment={-1}
-        onClick={handleRecordClick}
-      >
-        Prev Record
-      </button>
-      <button key="nextRecord"
-        data-increment={1}
-        onClick={handleRecordClick}
-      >
-        Next Record
-      </button>
-      <h4 className="no-margin">Record Index: {selectedRecordIndex}</h4>
-      <h4 className="no-margin">Record Id: {data.filter((d: any) => d.id === userIds[selectedUserIndex])[0].data[selectedRecordIndex].id}</h4>
+      <div className="container-sm text-center" style={{ maxWidth: "400px" }}>
+        <h4 className="no-margin display-none">
+          User Index: {selectedUserIndex}
+        </h4>
+        <h4 className="no-margin display-none">
+          User ID: {userIds[selectedUserIndex]}
+        </h4>
+
+        <div className="btn-group">
+          <button
+            className="btn btn-primary"
+            key="prevUser"
+            data-increment={-1}
+            onClick={handleUserClick}
+          >
+            Prev Patient
+          </button>
+          <button
+            className="btn btn-primary"
+            key="nextUser"
+            data-increment={1}
+            onClick={handleUserClick}
+          >
+            Next Patient
+          </button>
+        </div>
+        <div className="row align-items-center mt-2">
+          <div className="col-4">
+            <img
+              className="img-thumbnail"
+              src={"./images/" + userIds[selectedUserIndex] + ".jpg"}
+              alt={"User" + userIds[selectedUserIndex]}
+            />
+          </div>
+          <div className="col-8">
+            <UserGeneralInfo record={userData.data[0]} />
+          </div>
+        </div>
+        <div className="btn-group my-2">
+          <button
+            className="btn btn-outline-info"
+            key="prevRecord"
+            data-increment={-1}
+            onClick={handleRecordClick}
+          >
+            Prev Record
+          </button>
+          <button
+            className="btn btn-outline-info"
+            key="nextRecord"
+            data-increment={1}
+            onClick={handleRecordClick}
+          >
+            Next Record
+          </button>
+        </div>
+        <div className="row"> 
+          <div className="col-6">
+            <h4 className="text-info text-left">
+              <i className="fa-solid fa-info-circle"></i>
+              Record Info:
+            </h4>
+            <RecordInfo record={selectedRecord} />
+            <h4 className="no-margin display-none">
+              Record Index: {selectedRecordIndex}
+            </h4>
+            <h4 className="no-margin display-none">
+              Record Id:{selectedRecord.id}
+            </h4>
+            <h4 className="text-info no-margin text-left">
+              <i className="fa-solid fa-diagnoses"></i>Diagnosis:
+            </h4>
+            <RecordDiagnosis record={selectedRecord} />
+          </div>
+          <div className="col-6">
+            <h4 className="text-info no-margin text-left">
+              <i className="fa-solid fa-heart-pulse"></i>Vitals:
+            </h4>
+            <UserVitals record={selectedRecord} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,50 @@
 import reactLogo from "../assets/react.svg";
+
 export default function Header() {
   return (
-    <h2 style={{ textAlign: "center", marginBottom: "2px" }}>
+    <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-center mt-0 pt-0 mb-4">
       <a
+        className="navbar-brand"
         href="https://github.com/attila5287/hackerrank-react-medical-records"
         target="_blank"
       >
         <img src={reactLogo} className="logo" alt="React logo" />
+      </a>
+      <a
+        className="nav-link"
+        href="https://github.com/attila5287/hackerrank-react-medical-records"
+        target="_blank"
+      >
         Patient Medical Records
       </a>
-    </h2>
+      <a
+        className="nav-link"
+        href="https://github.com/attila5287/hackerrank-react-medical-records"
+        target="_blank"
+      >
+        <i className="fa-brands fa-github"></i>
+      </a>
+      <a
+        className="nav-link"
+        href="https://www.hackerrank.com/challenges/patient-medical-records/"
+        target="_blank"
+      >
+        <i className="fa-brands fa-hackerrank"></i>
+      </a>
+      <a
+        className="nav-link"
+        href="https://randomuser.me/api/?results=20"
+        target="_blank"
+      >
+        <i className="fa-solid fa-circle-user"></i>
+      </a>
+      <a
+        className="nav-link"
+        href="https://hackerrank-react-medical-records.vercel.app/"
+        target="_blank"
+      >
+        <i className="fa-solid fa-globe"></i>
+      </a>
+    </nav>
   );
 }
